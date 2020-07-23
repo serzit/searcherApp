209f278b81ebc903668fbf6c441ab93d9258cf61
serzit <serzit@yandex.ru>
2020-07-23 06:10:09 +0300

    Updates

---
+++ precache-manifest.d8980de8273d70ed47e351f8f9fbdd8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e41d4c288010c1d7e9cfeb78bb1bd4",
+    "url": "/searcherApp/index.html"
+  },
+  {
+    "revision": "442e722f516ca8fb427b",
+    "url": "/searcherApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee45b8ced740225a7a41",
+    "url": "/searcherApp/static/js/2.59bdb856.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/searcherApp/static/js/2.59bdb856.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "442e722f516ca8fb427b",
+    "url": "/searcherApp/static/js/main.cc98d42e.chunk.js"
+  },
+  {
+    "revision": "073577dee8cd16a31fa9",
+    "url": "/searcherApp/static/js/runtime-main.4b31b78b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/searcherApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
